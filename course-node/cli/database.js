968608770f66e165588a5fe831bc2307c6da0b41
jonathanccardoso <jonathan99moura@hotmail.com>
2021-05-21 15:50:30 -0300

    Fixme: adds function form cli script

--- course-node/cli/database.js
+++ course-node/cli/database.js
@@ -1,22 +1,73 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
 const { promisify } = require("util");
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
   constructor() {
-    this.NAME_FILE = "heroes.json";
+    this.NAME_FILE = "cli/heroes.json";
   }
   async getDataFile() {
     const file = await readFileAsync(this.NAME_FILE, "utf8");
 
     return JSON.parse(file.toString());
   }
-  writeFile() {}
+  async writeFile(data) {
+    await writeFileAsync(this.NAME_FILE, JSON.stringify(data));
+    return true;
+  }
+
+  async create(hero) {
+    const data = await this.getDataFile();
+    const id = hero.id <= 2 ? hero.id : Date.now();
+
+    const heroIdWithId = {
+      id,
+      ...hero,
+    };
+
+    const dataFinal = [...data, heroIdWithId];
+    const result = await this.writeFile(dataFinal);
+    return result;
+  }
+
   async list(id) {
     const data = await this.getDataFile();
     const dataFilter = data.filter((item) => (id ? item.id === id : true));
     return dataFilter;
+  }
+
+  async delete(id) {
+    if (!id) {
+      return await this.writeFile([]);
+    }
+
+    const data = await this.getDataFile();
+    const index = data.findIndex((item) => item.id === parseInt(id));
+
+    if (index === -1) {
+      throw Error("O usuário informado nao existe");
+    }
+
+    data.splice(index, 1);
+    return await this.writeFile(data);
+  }
+
+  async update(id, newData) {
+    const data = await this.getDataFile();
+    const index = data.findIndex((item) => item.id === parseInt(id));
+    if (index === -1) {
+      throw Error("The hero defined not exist");
+    }
+    const current = dada[index];
+    const objUpdated = {
+      ...current,
+      ...newData,
+    };
+    data.splice(index, 1);
+
+    return await this.writeFile([...data, objUpdated]);
   }
 }
 
--- course-node/cli/test.js
+++ course-node/cli/test.js
@@ -2,25 +2,56 @@ const { deepEqual, ok, deepStrictEqual } = require("assert");
 
 const database = require("./database");
 
-const DEFAULT_ITEM_CADASTRAR = {
+const DEFAULT_ITEM_CREATE = {
   id: 1,
   name: "Flash",
   power: "Speed",
 };
 
+const DEFAULT_ITEM_UPDATE = {
+  id: 1,
+  name: "Lanterna Verde",
+  power: "Energia do anel",
+};
+
 describe("Hero manipulation suite", () => {
+  before(async () => {
+    await database.create(DEFAULT_ITEM_CREATE);
+    await database.create(DEFAULT_ITEM_UPDATE);
+  });
+
   it("Must search a hero, using files", async () => {
-    const expected = DEFAULT_ITEM_CADASTRAR;
-    
-    // result is position 0
+    const expected = DEFAULT_ITEM_CREATE;
+    // result on position 0
     const [result] = await database.list(expected.id);
 
     deepStrictEqual(result, expected);
   });
 
-  // it("Must register a hero, using files", async () => {
-  //   const expected = DEFAULT_ITEM_CADASTRAR;
+  it("Must register a hero, using files", async () => {
+    const expected = DEFAULT_ITEM_CREATE;
+    const result = await database.create(DEFAULT_ITEM_CREATE);
+    const [actual] = await database.list(DEFAULT_ITEM_CREATE.id);
 
-  //   ok(null, expected);
-  // });
+    deepStrictEqual(actual, expected);
+  });
+
+  it("Must remove a hero by id", async () => {
+    const expected = true;
+    const result = await database.delete(DEFAULT_ITEM_CREATE.id);
+
+    deepStrictEqual(result, expected);
+  });
+
+  it("Must updated a hero by id", async () => {
+    const expected = { ...DEFAULT_ITEM_UPDATE, name: "Batman", power: "Money" };
+    const newData = {
+      name: "Batman",
+      power: "Money",
+    };
+
+    await database.update(DEFAULT_ITEM_UPDATE.id, expected);
+    const [result] = await database.list(DEFAULT_ITEM_UPDATE.id);
+    deepStrictEqual(result, expected);
+  });
 });
